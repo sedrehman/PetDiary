e8b24477220507ffc4bd59734e7da5bf9a9afd96
syed rehman <syedrehm@buffalo.edu>
2020-04-18 18:41:07 -0400

    fasdf

--- pet_diary.js
+++ pet_diary.js
@@ -47,17 +47,7 @@ app.get('/', function (req, res) {
 );
 
 app.post('/file-text', function(req, res) {
-  const post = models.feed.build({
-    id: req.session.id,
-    username: req.session.username,
-    name: req.name.file,
-    type: "text",
-    creation_time: Date.now()
-  })
-
-  post.save().then(function(post) {
-    console.log(post);
-  })
+	console.log(req.body);
 })
 
 app.post('/file-image', function(req, res) {
@@ -75,134 +65,34 @@ app.post('/file-image', function(req, res) {
 })
 
 app.post('/file-video', function(req, res) {
-  const post = models.feed.build({
-    id: req.session.id,
-    username: req.session.username,
-    name: req.name.file,
-    type: "video",
-    creation_time: Date.now()
-  })
+	const post = models.feed.build({
+		id: req.session.id,
+		username: req.session.username,
+		name: req.name.file,
+		type: "video",
+		creation_time: Date.now()
+	});
 
-  post.save().then(function(post) {
-    console.log(post);
-  })
+	post.save().then(function(post) {
+		console.log(post);
+	});
 })
 
 app.post('/create-comment', function(req, res) {
-  const post = models.feed.build({
-    id: req.session.id,
-    username: req.session.username,
-    name: req.name.file,
-    type: "video",
-    creation_time: Date.now()
-  })
+	const post = models.feed.build({
+		id: req.session.id,
+		username: req.session.username,
+		name: req.name.file,
+		type: "video",
+		creation_time: Date.now()
+	});
 
-  post.save().then(function(post) {
-    console.log(post);
-  })
+	post.save().then(function(post) {
+		console.log(post);
+	});
 })
 
-// app.get('/signUp.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/signUp.html")
-// }
-// );
 
-// app.get('/login.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/login.html")
-// }
-// );
-
-// app.get('/createFeed.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/createFeed.html")
-// }
-// );
-
-// app.get('/home.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/home.html")
-// }
-// );
-
-// app.get('/item.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/item.html")
-// }
-// );
-
-// app.get('/messages.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/messages.html")
-// }
-// );
-
-// app.get('/profile.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/profile.html")
-// }
-// );
-
-// app.get('/profile3.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/profile3.html")
-// }
-// );
-
-// app.get('/profile2.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/profile2.html")
-// }
-// );
-
-
-// app.get('/edit.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/edit.html")
-// }
-// );
-// app.get('/style.css', function (req, res) {
-//     res.sendFile(__dirname + "/website/style.css")
-// }
-// );
-
-// app.get('/Messenger.css', function (req, res) {
-//     res.sendFile(__dirname + "/website/Messenger.css")
-// }
-// );
-
-// app.get('/script.js', function (req, res) {
-//     res.sendFile(__dirname + "/website/script.js")
-// }
-// );
-
-// app.get('/images/banner.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/banner.jpg")
-// }
-// );
-
-
-// app.get('/images/bunham.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/bunham.jpg")
-// }
-// );
-
-// app.get('/images/cute_kitten.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/cute_kitten.jpg")
-
-// }
-// );
-
-// app.get('/images/cuteDuo.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/cuteDuo.jpg")
-// }
-// );
-
-// app.get('/images/cuteDuo.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/cuteDuo.jpg")
-// }
-// );
-
-// app.get('/Profile_placeholder.png', function (req, res) {
-//     res.sendFile(__dirname + "/website/Profile_placeholder.png")
-// }
-// );
-
-// app.get('/images/turtleparrot.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/turtleparrot.jpg")
-// }
-// );
 function removeBadChars(bad){
 	bad = bad.replace('<', '');
 	bad = bad.replace('>', '');

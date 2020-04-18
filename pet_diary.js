cc00d412f265b766c58acbd555c56a43e3bbd85c
syed rehman <syedrehm@buffalo.edu>
2020-04-18 12:11:38 -0400

    server working

--- pet_diary.js
+++ pet_diary.js
@@ -5,11 +5,21 @@ var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
 var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'pet_diary'
+	host:'localhost',
+	user:'phpmyadmin',
+	password:'hello123',
+	database:'pet_diary',
+	port:3306
 });
+
+connection.connect(function(err) {
+	if (err) {
+	  return console.error('error: ' + err.message);
+	}
+  
+	console.log('Connected to the MySQL server.');
+});
+
 
 const app = express();
 
@@ -19,9 +29,9 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({
-	secret: 'secret',
+	secret: 'cse312',
 	resave: true,
-	saveUninitialized: true
+	saveUninitialized: false
 }));
 
 const port = 8000;
@@ -35,24 +45,60 @@ app.get('/', function (req, res) {
     }
 );
 
-
+function removeBadChars(bad){
+	bad = bad.replace('<', '');
+	bad = bad.replace('>', '');
+	bad = bad.replace('\'', '');
+	bad = bad.replace('\"', '');
+	bad = bad.replace(' ', '');
+	bad = bad.replace('$', '');
+	bad = bad.replace('*', '');
+	bad = bad.replace('#', '');
+	bad = bad.replace('&', '');
+	bad = bad.replace('!', '');
+	bad = bad.replace('~', '');
+	bad = bad.replace('`', '');
+	bad = bad.replace('%', '');
+	bad = bad.replace('^', '');
+	bad = bad.replace('(', '');
+	bad = bad.replace(')', '');
+	bad = bad.replace('=', '');
+	bad = bad.replace('-', '');
+	bad = bad.replace('[', '');
+	bad = bad.replace(']', '');
+	bad = bad.replace('{', '');
+	bad = bad.replace('}', '');
+	bad = bad.replace(';', '');
+	bad = bad.replace(',', '');
+	bad = bad.replace('/', '');
+	bad = bad.replace('\\', '');
+	var good  = bad;
+	return good;
+}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
 app.post('/auth', function(request, response) {
-    var username = request.body.username;
-    username = username.replace("\"", "");
-    username = username.replace("\'", "");
-	var password = request.body.password;
+    var username = request.body["username"];
+	username = removeBadChars(username);
+	var password = request.body["password"];
+	password = removeBadChars(password);
+
+	console.log(username );
+	console.log(password);
+
 	if (username && password) {
-		connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				console.log("logged in! ");
-				response.redirect('/home');
-			} else {
+		request.session.user_id = "";
+		connection.query("SELECT * from users WHERE email=? AND password=?",[username, password], function(error, results, fields) {
+			console.log(results);
+			if(error){
 				response.send('Incorrect Username and/or Password!');
-			}			
+			}
+			else if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.user_id = results['id'];
+				console.log("logged in! ");
+				response.redirect('./home.html');
+			} 
 			response.end();
 		});
 	} else {
@@ -60,6 +106,8 @@ app.post('/auth', function(request, response) {
 		response.end();
 	}
 });
+
+
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 

3305d18ddcb7d53fe2f15751764ec006e8f84920
Gustavo Zanella <gdzanell@buffalo.edu>
2020-04-18 18:33:17 -0400

    html changes

--- pet_diary.js
+++ pet_diary.js
@@ -61,17 +61,15 @@ app.post('/file-text', function(req, res) {
 })
 
 app.post('/file-image', function(req, res) {
-  const post = models.feed.build({
-    id: req.session.id,
-    username: req.session.username,
-    name: req.name.file,
-    type: "image",
-    creation_time: Date.now()
-  })
+	var username=req.body.name;
+    connection.query("INSERT INTO `names` (name) VALUES (?)", username.toString(), function(err, result){
+        if(err) throw err;
+            console.log("record innserted into file-image");
+        });
+    res.send(username);
+});
 
-  post.save().then(function(post) {
-    console.log(post);
-  })
+res.send(username);
 })
 
 app.post('/file-video', function(req, res) {
@@ -102,107 +100,6 @@ app.post('/create-comment', function(req, res) {
   })
 })
 
-// app.get('/signUp.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/signUp.html")
-// }
-// );
-
-// app.get('/login.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/login.html")
-// }
-// );
-
-// app.get('/createFeed.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/createFeed.html")
-// }
-// );
-
-// app.get('/home.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/home.html")
-// }
-// );
-
-// app.get('/item.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/item.html")
-// }
-// );
-
-// app.get('/messages.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/messages.html")
-// }
-// );
-
-// app.get('/profile.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/profile.html")
-// }
-// );
-
-// app.get('/profile3.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/profile3.html")
-// }
-// );
-
-// app.get('/profile2.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/profile2.html")
-// }
-// );
-
-
-// app.get('/edit.html', function (req, res) {
-//     res.sendFile(__dirname + "/website/edit.html")
-// }
-// );
-// app.get('/style.css', function (req, res) {
-//     res.sendFile(__dirname + "/website/style.css")
-// }
-// );
-
-// app.get('/Messenger.css', function (req, res) {
-//     res.sendFile(__dirname + "/website/Messenger.css")
-// }
-// );
-
-// app.get('/script.js', function (req, res) {
-//     res.sendFile(__dirname + "/website/script.js")
-// }
-// );
-
-// app.get('/images/banner.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/banner.jpg")
-// }
-// );
-
-
-// app.get('/images/bunham.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/bunham.jpg")
-// }
-// );
-
-// app.get('/images/cute_kitten.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/cute_kitten.jpg")
-
-// }
-// );
-
-// app.get('/images/cuteDuo.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/cuteDuo.jpg")
-// }
-// );
-
-// app.get('/images/cuteDuo.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/cuteDuo.jpg")
-// }
-// );
-
-// app.get('/Profile_placeholder.png', function (req, res) {
-//     res.sendFile(__dirname + "/website/Profile_placeholder.png")
-// }
-// );
-
-// app.get('/images/turtleparrot.jpg', function (req, res) {
-//     res.sendFile(__dirname + "/images/turtleparrot.jpg")
-// }
-// );
 function removeBadChars(bad){
 	bad = bad.replace('<', '');
 	bad = bad.replace('>', '');
@@ -241,7 +138,7 @@ app.post('/auth', function(request, response) {
 	var password = request.body["password"];
 	password = removeBadChars(password);
 
-	console.log(username );
+	console.log(username);
 	console.log(password);
 
 	if (username && password) {

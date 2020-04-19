5450a086e19b70002c0d4be7cf374d5e6872d6eb
syed rehman <syedrehm@buffalo.edu>
2020-04-19 13:10:57 -0400

    ajax

--- website/script.js
+++ website/script.js
@@ -63,21 +63,40 @@ function sendPosts(hello){
 		likes = list[i].likes;
 		console.log(likes);
 		
-		if(list[i].type.localeCompare("text")==0)
+		if(list[i].type.localeCompare("text")==0){
 			content = list[i].content;
+		}
+			
 
-		else if(list[i].type.localeCompare("video")==0)
+		if(list[i].type.localeCompare("video")==0){
 			content = list[i].content;
+		}
+			
 
-		else(list[i].type.localeCompare("picture")==0)
+		if(list[i].type.localeCompare("img")==0){
 			imge = list[i].content;
-			content = "<img src=\"../images/"+imge+">"
+			content = "<img src=\""+ imge + "\">";
+			// content = "<img src="+imge+">";
+			console.log(content);
+		}
+			
 
-		post_body.innerHTML = post_body.innerHTML + "<div class=\"container\"><div class=\"card-group\"><div class=\"card col-12\"><div class=\"card-body d-flex flex-row\"><img src=\"Profile_placeholder.png\" class=\"rounded-circle mr-3\" height=\"70px\" width=\"70px\" alt=\"avatar\"><div><h4 class=\"card-title font-weight-bold mb-2\"><a href = \"profile2.html\">"+un+"</a></h4></div></div><hr><div class=\"card-body\"><p class=\"card-text\">"+content+"</p></div><div class=\"btn-group btn-group-toggle\" data-toggle=\"buttons\"><label class=\"btn btn-link	 col-7\"><a href=\"item.html\" class=\"btn form-control btn-dark\">View Post</a></label><label class=\"btn btn-link col-3\"><button class=\"btn btn-block btn-success\"><i class=\"fa fa-thumbs-up\">Like</i> </button></label><label class=\"btn btn-link col-2\"><input type=\"\" class=\"form-control\" id=\"likes\" placeholder="+likes+"></label></div></div></</div>"
+
+		post_body.innerHTML = post_body.innerHTML + "<div class=\"container\"><div class=\"card-group\"><div class=\"card col-12\"><div class=\"card-body d-flex flex-row\"><img src=\"Profile_placeholder.png\" class=\"rounded-circle mr-3\" height=\"70px\" width=\"70px\" alt=\"avatar\"><div><h4 class=\"card-title font-weight-bold mb-2\"><a href = \"profile2.html\">"+un+"</a></h4></div></div><hr><div class=\"card-body\"><p class=\"card-text\">"+content+"</p></div><div class=\"btn-group btn-group-toggle\" data-toggle=\"buttons\"><label class=\"btn btn-link	 col-7\"><a href=\"item.html\" class=\"btn form-control btn-dark\">View Post</a></label><label class=\"btn btn-link col-3\"><button class=\"btn btn-block btn-success\"><i class=\"fa fa-thumbs-up\">Like</i> </button></label><label class=\"btn btn-link col-2\"><label type=\"\" class=\"form-control\" id=\"likes\" placeholder="+likes+"> </label></label></div></div></</div>"
 	}
 }
+/*
+function test(){
+	var i
+	for(i = 0; i<10;i++)
+	{
+		un = "bob";
+		content = "text is here";
+		x.innerHTML = x.innerHTML + "<div class=\"container\"><div class=\"card-group\"><div class=\"card col-12\"><div class=\"card-body d-flex flex-row\"><img src=\"Profile_placeholder.png\" class=\"rounded-circle mr-3\" height=\"70px\" width=\"70px\" alt=\"avatar\"><div><h4 class=\"card-title font-weight-bold mb-2\"><a href = \"profile2.html\">"+un+"</a></h4></div></div><hr><div class=\"card-body\"><p class=\"card-text\">"+content+"</p></div><div class=\"btn-group btn-group-toggle\" data-toggle=\"buttons\"><label class=\"btn btn-link	 col-7\"><a href=\"item.html\" class=\"btn form-control btn-dark\">View Post</a></label><label class=\"btn btn-link col-3  \"><button class=\"btn btn-block btn-success\"><i class=\"fa fa-thumbs-up\">Like</i> </button></label><label class=\"btn btn-link col-2\"><input type=\"\" class=\"form-control\" id=\"likes\" placeholder=\"0\"></label></div></div></div>"
+	}
+}*/
 
-function sendLike(){
+function sendLike(feed_id, number_of_like){
 	request = new XMLHttpRequest();
 	request.onreadystatechange = function(){	
 	    if	(this.readyState	===	4	&&	this.status	===	200){	

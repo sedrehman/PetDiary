eeed7bdcd18cc3b41937ca62ad54cb7aefbb6536
Gustavo Zanella <gdzanell@buffalo.edu>
2020-04-19 11:12:02 -0400

    fixed like look

--- website/script.js
+++ website/script.js
@@ -25,21 +25,21 @@ function getPosts(){
 	var i;
 	var list = [];
 	var dictionary = getSQL();
-	
-	for (i = 0; i < dictionary.length; i++) 
+
+	for (i = 0; i < dictionary.length; i++)
 	{
 		x = new post(dictionary[i].user_name,dictionary[i].type,dictionary[i].name);
 		list.push(post)
 	}
-	
+
 	return list;
 }
 
 function sendPosts(){
 	/*list = getPosts()
 	var i;
-	var un 
-	var content 
+	var un
+	var content
 	for(i = 0; i<list.length;i++)
 	{
 		un = list[i].un;
@@ -48,17 +48,17 @@ function sendPosts(){
 		else if(list[i].type.localeCompare("video")
 			content = list[i].content;
 		else(list[i].type.localeCompare("picture")
-			content = list[i].content;		
+			content = list[i].content;
 		x.innerHTML = x.innerHTML + "<div class=\"container\"><div class=\"row col-12\"><div class=\"card-group\"><div class=\"card col-12\"><div class=\"card-body d-flex flex-row\"><img src=\"Profile_placeholder.png\" class=\"rounded-circle mr-3\" height=\"70px\" width=\"70px\" alt=\"avatar\"><div><h4 class=\"card-title font-weight-bold mb-2\"><a href = \"profile2.html\">"+un+"</a></h4></div></div><hr><div class=\"card-body\"><p class=\"card-text\">"+content+"</p></div><div class=\"btn-group btn-group-toggle\" data-toggle=\"buttons\"><label class=\"btn btn-link	 col-9\"><a href=\"item.html\" class=\"btn form-control btn-dark\">View Post</a></label><label class=\"btn btn-link col-2\"><button class=\"btn btn-block btn-success\"><i class=\"fa fa-thumbs-up\">Like</i> </button></label><label class=\"btn btn-link col-1\"><input type=\"\" class=\"form-control\" id=\"likes\" placeholder=\"0\"></label></div></div></div></</div>"
 	}*/
 }
 
 function test(){
-	var i 
+	var i
 	for(i = 0; i<10;i++)
 	{
 		un = "bob";
-		content = "text is here";		
-		x.innerHTML = x.innerHTML + "<div class=\"container\"><div class=\"row col-12\"><div class=\"card-group\"><div class=\"card col-12\"><div class=\"card-body d-flex flex-row\"><img src=\"Profile_placeholder.png\" class=\"rounded-circle mr-3\" height=\"70px\" width=\"70px\" alt=\"avatar\"><div><h4 class=\"card-title font-weight-bold mb-2\"><a href = \"profile2.html\">"+un+"</a></h4></div></div><hr><div class=\"card-body\"><p class=\"card-text\">"+content+"</p></div><div class=\"btn-group btn-group-toggle\" data-toggle=\"buttons\"><label class=\"btn btn-link	 col-9\"><a href=\"item.html\" class=\"btn form-control btn-dark\">View Post</a></label><label class=\"btn btn-link col-2\"><button class=\"btn btn-block btn-success\"><i class=\"fa fa-thumbs-up\">Like</i> </button></label><label class=\"btn btn-link col-1\"><input type=\"\" class=\"form-control\" id=\"likes\" placeholder=\"0\"></label></div></div></div></</div>"
+		content = "text is here";
+		x.innerHTML = x.innerHTML + "<div class=\"container\"><div class=\"row col-12\"><div class=\"card-group\"><div class=\"card col-12\"><div class=\"card-body d-flex flex-row\"><img src=\"Profile_placeholder.png\" class=\"rounded-circle mr-3\" height=\"70px\" width=\"70px\" alt=\"avatar\"><div><h4 class=\"card-title font-weight-bold mb-2\"><a href = \"profile2.html\">"+un+"</a></h4></div></div><hr><div class=\"card-body\"><p class=\"card-text\">"+content+"</p></div><div class=\"btn-group btn-group-toggle\" data-toggle=\"buttons\"><label class=\"btn btn-link	 col-8\"><a href=\"item.html\" class=\"btn form-control btn-dark\">View Post</a></label><label class=\"btn btn-link col-3\"><button class=\"btn btn-block btn-success\"><i class=\"fa fa-thumbs-up\">Like</i> </button></label><label class=\"btn btn-link col\"><input type=\"\" class=\"form-control\" id=\"likes\" placeholder=\"0\"></label></div></div></div></</div>"
 	}
 }

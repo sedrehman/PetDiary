4cf6c1cf19dfdfdb3c10c843d6cb8a2b47b004af
jonhee <johngoo1400@gmail.com>
2020-05-10 21:58:38 -0400

    xzczxc
    xzcxzcxzcxz

--- website/chat.js
+++ website/chat.js
@@ -29,8 +29,6 @@ function getUser(){
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function(){	
 		if	(this.readyState === 4 && this.status === 200){	
-			//console.log(JSON.parse(this.response));
-			console.log(this.response);
 			bob = this.response.split("######");
 			user = new person(bob[0],bob[1])
 		}	
@@ -43,7 +41,6 @@ function getFriends(){
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function(){	
 		if(this.readyState === 4 && this.status === 200){	
-			console.log(this.response);
 			parseFriends(this.response);
 
 			createFriends();
@@ -54,13 +51,8 @@ function getFriends(){
 }
 
 function createFriends(){
-	//console.log(friends);
-	//console.log(friends.length);
-	console.log("#############################################");
-	console.log(friends.length);
 	box = document.getElementById("friendContainer");
 	for(var i = 0; i < friends.length; i++){
-		console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		b = document.createElement('span');
 		b.innerHTML = "<div id = \"ha\" onclick = \"loadNewMessages('"+friends[i].id+"')\"><div id = \"friend\">"+friends[i].name+"</div></div>";
 		box.appendChild(b);
@@ -68,7 +60,6 @@ function createFriends(){
 }
 
 function loadNewMessages(newC){
-	alert(newC);
 	for(i =0;i<friends.length;i++)
 	{
 		if(newC == friends[i].id)
@@ -77,14 +68,13 @@ function loadNewMessages(newC){
 			break;
 		}
 	}
+	getMessages()
 }
 
 function parseFriends(sup){
 	var one = sup.split("######");
-	console.log(one);
 	for(var i =0;i<one.length;i++)
 	{
-		console.log(i + "~~~");
 		var hi = one[i].split("@@@");
 		friends.push(new person(hi[0],hi[1]));
 	}
